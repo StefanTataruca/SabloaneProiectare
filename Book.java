3486d2cac49edc2636f438cfd33474cbc6788ba9
StefanTataruca <79994789+StefanTataruca@users.noreply.github.com>
2023-10-17 08:21:20 +0300

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,28 +1,35 @@
-class Book(object):
-    paragraphs=[]
-    images=[]
-    tables=[]
-    title=""
-    def __init__(self, s):
-        self.title=s
-    def createNewParagraph(self, a):
-        self.paragraphs.append(a)
-    def createNewImage(self, a):
-        self.images.append(a)
-    def createNewTable(self, a):
-        self.tables.append(a)
-    def print(self):
-        print(self.title)
-        print(self.paragraphs)
-        print(self.tables)
-        print(self.images)
+import java.util.ArrayList;
+import java.util.List;
 
-discoTitanic = Book("Disco Titanic")
-discoTitanic.createNewParagraph("Paragraph 1")
-discoTitanic.createNewParagraph("Paragraph 2")
-discoTitanic.createNewParagraph("Paragraph 3")
-discoTitanic.createNewImage("Image 1")
-discoTitanic.createNewParagraph("Paragraph 4")
-discoTitanic.createNewTable("Table 1")
-discoTitanic.createNewParagraph("Paragraph 5")
-discoTitanic.print()
+public class Book {
+	String title;
+	List<String> paragraphs = new ArrayList<>();
+	List<String> images = new ArrayList<>();
+	List<String> tables = new ArrayList<>();
+	
+	public Book(String title) {
+		this.title=title;
+		
+	};
+	
+	public void print(){
+		System.out.println(title);
+		System.out.println(paragraphs);
+		System.out.println(images);
+		System.out.println(tables);
+	}
+	
+	 public void createNewParagraph(String paragraph) {
+		 paragraphs.add(paragraph);
+		 
+	 }
+	 
+	 public void createNewImage(String image) {
+		 images.add(image);
+	 }
+	 
+	 public void createNewTable(String table) {
+		 tables.add(table);
+		 
+	 }
+}
