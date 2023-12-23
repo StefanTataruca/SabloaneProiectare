1f6fceb824ba536eaa8231a2c1fa8e8619e723d2
StefanTataruca <79994789+StefanTataruca@users.noreply.github.com>
2023-12-23 13:24:07 +0200

    Add files via upload

---
+++ LAB6/Sabloane/src/main/java/com/example/sabloane/AlignRight.java
@@ -0,0 +1,7 @@
+package com.example.sabloane;
+
+public class AlignRight implements AlignStrategy {
+    public String align(String text) {
+        return "Right-aligned: " + text;
+    }
+}
---
+++ LAB6/Sabloane/src/main/java/com/example/sabloane/AlignStrategy.java
@@ -0,0 +1,5 @@
+package com.example.sabloane;
+
+public interface AlignStrategy {
+    String align(String text);
+}
